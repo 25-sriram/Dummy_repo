1fa730541cfef61fd0a17a258bb1b922b7b25150
Sri Ram Rajendran <shakthi.ram0713@gmail.com>
2026-03-14 17:42:23 +0530

    For amother Clarification

--- nanthasuper.py
+++ nanthasuper.py
@@ -1,3 +1,6 @@
 print("Nantha is Super")
 print("Nantha is Super")
 print("Nantha is Super")
+print("Nantha is Super")
+print("Nantha is Super")
+print("Nantha is Super")
--- sriramdummy.py
+++ sriramdummy.py
@@ -1,1 +1,4 @@
 print("Sri is dummy")
+print("Nantha is Super")
+print("Nantha is Super")
+print("Nantha is Super")
